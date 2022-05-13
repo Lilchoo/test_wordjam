b1f6d284733f65213aa304688032bfaab64aa605
Gareth Ng <94983540+Lilchoo@users.noreply.github.com>
2022-05-13 14:20:17 -0700

    Add files via upload

---
+++ public/js/skeleton.js
@@ -0,0 +1,8 @@
+
+function loadSkeleton() {
+    console.log($('#headerPlaceholder').load('js/header.html'));
+    console.log($('#footerPlaceholder').load('js/footer.html'));
+}
+
+
+loadSkeleton();
